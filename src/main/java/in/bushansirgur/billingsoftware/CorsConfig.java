44b38901fe7eca3989e5c2af343ea08c51fca385
Aditya-run <adityast473@gmail.com>
2025-07-21 01:13:35 +0530

    updated the CorsConfig.java

---
+++ src/main/java/in/bushansirgur/billingsoftware/CorsConfig.java
@@ -0,0 +1,18 @@
+package in.bushansirgur.billingsoftware;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://medibill-frontend.vercel.app")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true); // optional
+    }
+}
